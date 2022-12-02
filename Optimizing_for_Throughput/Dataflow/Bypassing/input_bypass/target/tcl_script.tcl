02333217420d68101b4f7b76a864a77e1c9a1f49
Maxwellhyh <994836613@qq.com>
2022-12-02 23:59:51 +0800

    1202

---
+++ Optimizing_for_Throughput/Dataflow/Bypassing/input_bypass/target/tcl_script.tcl
@@ -0,0 +1,22 @@
+set Project     input_bypass_sol
+set Solution    solution1
+set Device      "xc7z020-clg400-1"
+set Flow        "vivado"
+set Clock       10
+
+open_project $Project -reset
+
+set_top dut
+
+add_files dut_sol.cpp -cflags -I.
+add_files -tb test_dut.cpp -cflags -I. 
+
+open_solution -flow_target $Flow -reset $Solution
+set_part $Device
+create_clock -period $Clock
+
+csim_design
+csynth_design
+cosim_design
+
+exit
