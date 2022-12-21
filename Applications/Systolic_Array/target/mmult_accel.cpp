d9eb241e69c81f31a8ca42449279b75447dfd755
Maxwellhyh <994836613@qq.com>
2022-12-21 22:48:05 +0800

    1221

--- Applications/Systolic_Array/target/mmult_accel.cpp
+++ Applications/Systolic_Array/target/mmult_accel.cpp
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include "mmult_accel.h"
 #define block 8
-void mmult(int* A, int* B, int* C);
 
 void mmult_hw(int* in1, int* in2, int* out)
 {
@@ -19,41 +18,31 @@ void mmult_hw(int* in1, int* in2, int* out)
                 outsum[k]=0;
             }
             for (int m=0; m< DATA_SIZE; m +=block){
-                int *in1block = in1 + DATA_SIZE*i + m;
-                int *in2block = in2 + DATA_SIZE*m + j;
-                mmult(in1block, in2block, outbuf);
+
+                systolic1: for(int k = 0; k < block; k++) {
+                    #pragma HLS PIPELINE
+                        systolic2: for(int ii = 0; ii < block; ii++) {
+                            systolic3: for(int jj = 0; jj < block; jj++) {
+                                int last = (k==0) ? 0 : outbuf[ii*block+jj];
+
+                                int a_val = (ii < block && k < block)? in1[DATA_SIZE*i + m + ii*DATA_SIZE+k] : 0;
+                                int b_val = (k < block && jj < block)? in2[DATA_SIZE*m + j + k*DATA_SIZE+jj] : 0;
+                                int result = last + a_val*b_val;
+
+                                outbuf[ii*block+jj] = result;
+                            }
+                        }
+                    }
                 for (int t = 0; t < block*block; t++){
                 #pragma HLS unroll
                     outsum[t] += outbuf[t];
                 }
             }
-            int* outblock = out + DATA_SIZE*i +j;
 
             for(int  k=0; k < block; k++){
                 for(int l=0; l < block; l++){
-                    outblock[DATA_SIZE*k+l] = outsum[block*k+l];
+                    out[DATA_SIZE*i + j + DATA_SIZE*k+l] = outsum[block*k+l];
                 }
-            }
-        }
-    }
-}
-
-
-
-void mmult(int* A, int* B, int* C)
-{
-
-    systolic1: for(int k = 0; k < block; k++) {
-    #pragma HLS PIPELINE
-        systolic2: for(int i = 0; i < block; i++) {
-            systolic3: for(int j = 0; j < block; j++) {
-                int last = (k==0) ? 0 : C[i*block+j];
-
-                int a_val = (i < block && k < block)? A[i*DATA_SIZE+k] : 0;
-                int b_val = (k < block && j < block)? B[k*DATA_SIZE+j] : 0;
-                int result = last + a_val*b_val;
-
-                C[i*block+j] = result;
             }
         }
     }
